218228ea43cee1638949871ddb2acd5e755f9547
Ian Miller <theiandmiller@gmail.com>
2025-01-02 13:54:11 -0600

    hashed pw

--- models/user.go
+++ models/user.go
@@ -1,6 +1,9 @@
 package models
 
-import "example.com/gogo/db"
+import (
+	"example.com/gogo/db"
+	"example.com/gogo/utils"
+)
 
 type User struct {
 	ID int64 
@@ -20,7 +23,12 @@ func (u User) Save() error {
 
 	defer stmt.Close()
 
-	result, err := stmt.Exec(u.Email, u.Password)
+	hashedPassword, err := utils.HashPassword(u.Password)
+	if err != nil {
+		return err
+	}
+
+	result, err := stmt.Exec(u.Email, hashedPassword)
 	if err != nil {
 		return err
 	}
