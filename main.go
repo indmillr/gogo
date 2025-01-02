2aef156d0e700d99afb814b5d2e27621a5fe646b
Ian Miller <theiandmiller@gmail.com>
2025-01-02 10:52:57 -0600

    include http server

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Print("Hello World!")
+	// --- Using Gin package for http server with Logger and Recovery middleware attached
+	server := gin.Default()
+
+	// --- handler for incoming req
+	server.GET("/events", getEvents)
+
+	// --- listen for incoming requests (localhost)
+	server.Run(":8080")
+}
+
+// ----- For GET Reqs...
+func getEvents(context *gin.Context) {
+	context.JSON(http.StatusOK, gin.H{"message": "Hello!"})
 }
