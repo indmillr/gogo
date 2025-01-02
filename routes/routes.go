6debc4a7840630bae2fa6fdbbeeede7a0f027cdb
Ian Miller <theiandmiller@gmail.com>
2025-01-02 13:22:52 -0600

    update Events

--- routes/routes.go
+++ routes/routes.go
@@ -2,9 +2,12 @@ package routes
 
 import "github.com/gin-gonic/gin"
 
+// --- handlers (routes) for incoming requests ([endpoint], [action])
 func RegisterRoutes(server *gin.Engine) {
-	// --- handlers (routes) for incoming requests ([endpoint], [action])
 	server.GET("/events", getEvents)
 	server.GET("/events/:id", getEvent)
+
 	server.POST("/events", createEvent)
+
+	server.PUT("/events/:id", updateEvent)
 }
