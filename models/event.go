9c95fced65f423390dfd8c722ee5a0ee655a8a27
Ian Miller <theiandmiller@gmail.com>
2025-01-02 11:55:57 -0600

    store items in db

--- models/event.go
+++ models/event.go
@@ -1,9 +1,13 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"example.com/gogo/db"
+)
 
 type Event struct {
-	ID int
+	ID int64
 	Name string `binding:"required"`
 	Description string `binding:"required"`
 	Location string `binding:"required"`
@@ -14,9 +18,29 @@ type Event struct {
 var events = []Event{}
 
 // ----- SAVE
-func (e Event) Save() {
-	// --- TODO: add to DB
-	events = append(events, e)
+func (e Event) Save() error {
+	// --- Question Marks are used in query strings to help with sanitation
+	query := `
+	INSERT INTO events(name, description, location, dateTime, user_id)
+	VALUES (?, ?, ?, ?, ?)`
+	
+	stmt, err := db.DB.Prepare(query)
+	if err != nil {
+		return err
+	}
+	
+	// --- defer Close of stmt to end of function
+	defer stmt.Close()
+
+	// --- Execute stmt using 'query' where Question Marks are replaced by values passed to stmt.Exec() below
+	result, err := stmt.Exec(e.Name, e.Description, e.Location, e.DateTime, e.UserID)
+	if err != nil {
+		return err
+	}
+	// --- Get the ID of the most recently inserted data
+	id, err := result.LastInsertId()
+	e.ID = id
+	return err
 }
 
 // ----- Get all available Events
